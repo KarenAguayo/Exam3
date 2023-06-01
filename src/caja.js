3f51e5752f2e1fc677e7a9f5bd43801e0dcfcf02
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-06-01 10:02:43 -0400

    INTENTANDO EL ALERT

---
+++ src/caja.js
@@ -0,0 +1,8 @@
+function Cambio(montoVenta,efectivo) {
+    if(postear(montoVenta.value==null,efectivo.value==null))
+    {
+        alert("ingrese los cambos");
+    }
+  }
+  
+  export default Cambio;
