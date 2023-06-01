158e11b859022c37819a426de3943c5719c68afb
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-06-01 09:41:29 -0400

    PRUEBA 2 TDD PASANDO

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,4 +4,7 @@ describe("Cambio", () => {
   it("deberia calcular el cambio", () => {
     expect(cambio(3, 2)).toEqual(1);
   });
+  it("venta deberia ser menor a efectivo", () =>{
+    expect(cambio(2, 3)).toEqual(-1);
+  })
 });
